dbcd637f65bbf8c61b87e6643819c91a96eb7bd0
mel22083 <mel22083@uvg.edu.gt>
2026-04-22 20:52:11 -0500

    Create menu_image.h
    .h del arreglo de la imagen del menú del juego

---
+++ menu_image.h
@@ -0,0 +1,15 @@
+/*
+ * menu_image.h
+ *
+ *  Created on: Apr 19, 2026
+ *      Author: willi
+ */
+
+#ifndef SRC_MENU_IMAGE_H_
+#define SRC_MENU_IMAGE_H_
+
+#include <stdint.h>
+
+extern const uint16_t menu_map[];
+extern const unsigned int menu_map_len;
+#endif /* SRC_MENU_IMAGE_H_ */
